a3ee18b905f21f0b0352278be56ac4fe7e0c4e11
RemyDekor <remy.duval@hotmail.fr>
2016-09-14 16:51:41 +0200

    Mise en place du menu et centrage du contenu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,35 @@
+
+
 $(document).ready(function() {
-    $('div').mouseenter(function() {
-        $('div').fadeTo('fast', 1);
+
+  // var categories = ['.illus', '.photo', '.design', '.code'];
+  //
+  // for (var i = 0 ; i < 4 ; i++) {
+  //   $('.categories ' + categories[i]).click(function() {
+  //   /*    $('.thumbnail').fadeTo('fast', 0.2);
+  //       $('.works ' + categories[i]).fadeTo(0, 1);  */
+  //       console.log('categories[i] est : ' + categories[i]);
+  //   });
+  // }
+    $('.categories .illus').click(function() {
+        $('.thumbnail').fadeTo('fast', 0.2);
+        $('.works .illus').fadeTo(0, 1);
     });
-    $('div').mouseleave(function() {
-        $('div').fadeTo('fast', 0.5);
+    $('.categories .photo').click(function() {
+        $('.thumbnail').fadeTo('fast', 0.2);
+        $('.works .photo').fadeTo(0, 1);
     });
+    $('.categories .design').click(function() {
+        $('.thumbnail').fadeTo('fast', 0.2);
+        $('.works .design').fadeTo(0, 1);
+    });
+    $('.categories .code').click(function() {
+        $('.thumbnail').fadeTo('fast', 0.2);
+        $('.works .code').fadeTo(0, 1);
+    });
+    $('.logo').click(function() {
+        $('.thumbnail').fadeTo('fast', 1);
+    });
+
+
 });
