bf0842599b7d508a825265c4702540d7a05ee13b
RemyDekor <remy.duval@hotmail.fr>
2016-11-08 16:28:12 +0100

    Commented-out Ritz and fixed jQuery on menu

--- js/script.js
+++ js/script.js
@@ -20,31 +20,40 @@ $(document).ready(function() {
 
     $('.categories .illus').click(function() {
         $('.categories p').not(this).css('text-shadow','none');
+        $('.categories p').not(this).css('color','#222');
         $(this).css('text-shadow','0px 0px 9px #000, 0px 2px 4px #000');
+        $(this).css('color','#fff');
         $('.thumbnail .mask').not('.illus .mask').fadeTo('fast', 1);
         $('.works .illus .mask').fadeTo('fast', 0);
     });
     $('.categories .photo').click(function() {
         $('.categories p').not(this).css('text-shadow','none');
+        $('.categories p').not(this).css('color','#222');
         $(this).css('text-shadow','0px 0px 9px #000, 0px 2px 4px #000');
+        $(this).css('color','#fff');
         $('.thumbnail .mask').not('.photo .mask').fadeTo('fast', 1);
         $('.works .photo .mask').fadeTo('fast', 0);
     });
     $('.categories .design').click(function() {
         $('.categories p').not(this).css('text-shadow','none');
+        $('.categories p').not(this).css('color','#222');
         $(this).css('text-shadow','0px 0px 9px #000, 0px 2px 4px #000');
+        $(this).css('color','#fff');
         $('.thumbnail .mask').not('.design .mask').fadeTo('fast', 1);
         $('.works .design .mask').fadeTo('fast', 0);
     });
     $('.categories .code').click(function() {
         $('.categories p').not(this).css('text-shadow','none');
+        $('.categories p').not(this).css('color','#222');
         $(this).css('text-shadow','0px 0px 9px #000, 0px 2px 4px #000');
+        $(this).css('color','#fff');
         $('.thumbnail .mask').not('.code .mask').fadeTo('fast', 1);
         $('.works .code .mask').fadeTo('fast', 0);
     });
     $('#logo').click(function() {
         // $('.thumbnail').fadeTo('fast', 1);
         $('.categories p').css('text-shadow','none');
+        $('.categories p').css('color','#222');
         $('.mask').fadeTo('fast', 0);
     });
 
