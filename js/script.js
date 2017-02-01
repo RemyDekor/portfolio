d01b47cb6ba797f96425fe40c2e30d7e5910402b
RemyDekor <remy.duval@hotmail.fr>
2017-02-01 09:49:51 +0100

    Fixing height problem when changing orientation on tab

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
 
  /* vvvvvvvvvv TROUVER MEILLEURE SOLUTION vvvvvvvvv */
   $(window).resize(function() {
-    if ($('container').height() < $(window).height()) {
+    if ($('container').height() < $(window).height()
+        || $('body').height() < $(window).height() ) {
       $('body').height($(window).height());
     }
     if ($(window).height() < $('body').height()) {
