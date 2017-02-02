f8ef189bcbc7619d32eb1ac2d124b70201345e00
RemyDekor <remy.duval@hotmail.fr>
2017-02-02 13:50:56 +0100

    Trying to prevent adress bar from showing inside projects

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 $(document).ready(function() {
 
 
-  $('body').on('click', '.ppp-container', function (){
+  $('body').on('scroll', '.ppp-container', function (){
         alert('click!');
     });
 
