2533b1c6053b8988779a487811d1796ea29490c4
remydekor <remyj.duval@gmail.com>
2017-11-07 22:47:08 +0100

    setting lights for all scenes

--- Flipfly/scripts/main-component.js
+++ Flipfly/scripts/main-component.js
@@ -382,6 +382,7 @@ AFRAME.registerComponent('main', {
                 ////CHANGING LIGHTS///////////////////////////////////////////////////
                 document.querySelector('#mainDirLight').setAttribute('color', '#f3cec6');
                 document.querySelector('#mainDirLight').setAttribute('intensity', '0.6');
+                document.querySelector('#directionaltarget').setAttribute('position', '-1 0 0');
                 document.querySelector('#mainAmbiantLight').setAttribute('light', 'type: ambient; color: #312980');
                 ///////////////////////////////////////////////////
                 }, 1000);
@@ -462,7 +463,7 @@ AFRAME.registerComponent('main', {
               ////CHANGING LIGHTS///////////////////////////////////////////////////
               document.querySelector('#mainDirLight').setAttribute('color', '#e4f9ff');
               document.querySelector('#mainDirLight').setAttribute('intensity', '0.9');
-              document.querySelector('#mainDirLight').setAttribute('position', '-1 0 1');
+              document.querySelector('#directionaltarget').setAttribute('position', '-1 0 1');
               document.querySelector('#mainAmbiantLight').setAttribute('light', 'type: ambient; color: #2d3339');
               ///////////////////////////////////////////////////
             }, 2000);
@@ -533,6 +534,7 @@ AFRAME.registerComponent('main', {
               ////CHANGING LIGHTS///////////////////////////////////////////////////
               document.querySelector('#mainDirLight').setAttribute('color', '#535353');
               document.querySelector('#mainDirLight').setAttribute('intensity', '0.2');
+              document.querySelector('#directionaltarget').setAttribute('position', '0 0 1');
               document.querySelector('#mainAmbiantLight').setAttribute('light', 'type: ambient; color: #2c2c2c');
               ///////////////////////////////////////////////////
             }, 2000);
@@ -599,6 +601,7 @@ AFRAME.registerComponent('main', {
               ////CHANGING LIGHTS///////////////////////////////////////////////////
               document.querySelector('#mainDirLight').setAttribute('color', '#fec168');
               document.querySelector('#mainDirLight').setAttribute('intensity', '0.6');
+              document.querySelector('#directionaltarget').setAttribute('position', '1 0 0');
               document.querySelector('#mainAmbiantLight').setAttribute('light', 'type: ambient; color: #663380');
               ///////////////////////////////////////////////////
             }, 1600);
@@ -657,6 +660,7 @@ AFRAME.registerComponent('main', {
               ////CHANGING LIGHTS///////////////////////////////////////////////////
               document.querySelector('#mainDirLight').setAttribute('color', '#7ea09f');
               document.querySelector('#mainDirLight').setAttribute('intensity', '0.1');
+              document.querySelector('#directionaltarget').setAttribute('position', '0 0 1');
               document.querySelector('#mainAmbiantLight').setAttribute('light', 'type: ambient; color: #100e1b');
               ///////////////////////////////////////////////////
             }, 1600);
