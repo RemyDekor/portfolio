e6f82290a8a3264296c186454dc76099d8f5381c
remydekor <remyj.duval@gmail.com>
2018-06-13 09:14:14 +0200

    new folders for portfolio v2

---
+++ portfoliov2/README.md
@@ -0,0 +1,29 @@
+# Portfolio
+Building my portfolio and learning Github at the same time.
+
+# HTTP-SERVER en local
+http-server package
+
+```bash
+$ npm install -g http-server # if not installed already
+$ http-server # runs a local server in the current folder
+$ http-server ./my-project # to run in a specific folder
+```
+
+it should print something like
+
+```
+Starting up http-server, serving ./
+Available on:
+  http://192.168.10.5:8080
+  http://127.0.0.1:8080
+Hit CTRL-C to stop the server
+```
+
+```javascript
+let toto = tutu;
+```
+
+# ESlint
+
+Bien séparer js et html pour qu'il fonctionne correctement
---
+++ portfoliov2/js/script.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+
+      $('#rectListener').on('click', function() {
+          resetThumbnailsDisplay();
+      });
+      $('.this').not('.nb').on('click', function() {
+          resetThumbnailsDisplay();
+      });
+
+    $('.categories .illus').on('click', function() {
+      if ($(this).hasClass('activ')) {
+        $('.thumbnail').not('.illus').toggleClass('hidden');
+        $('.works .illus').removeClass('hidden');
+      }else{
+        $('.thumbnail .illus').not('.nb').removeClass('hidden');
+        $('.thumbnail').not('.illus').addClass('hidden');
+      }
+      $(this).toggleClass('activ');
+      $('.categories p').not(this).removeClass('activ');
+      $('.works .illus').removeClass('hidden');
+    });
+
+    $('.categories .photo').on('click', function() {
+      if ($(this).hasClass('activ')) {
+        $('.thumbnail').not('.photo').toggleClass('hidden');
+        $('.works .photo').removeClass('hidden');
+      }else{
+        $('.thumbnail .photo').not('.nb').removeClass('hidden');
+        $('.thumbnail').not('.photo').addClass('hidden');
+      }
+      $(this).toggleClass('activ');
+      $('.categories p').not(this).removeClass('activ');
+      $('.works .photo').removeClass('hidden');
+    });
+
+    $('.categories .design').on('click', function() {
+      if ($(this).hasClass('activ')) {
+        $('.thumbnail').not('.design').toggleClass('hidden');
+        $('.works .design').removeClass('hidden');
+      }else{
+        $('.thumbnail .design').not('.nb').removeClass('hidden');
+        $('.thumbnail').not('.design').addClass('hidden');
+      }
+      $(this).toggleClass('activ');
+      $('.categories p').not(this).removeClass('activ');
+      $('.works .design').removeClass('hidden');
+    });
+
+    $('.categories .code').on('click', function() {
+      if ($(this).hasClass('activ')) {
+        $('.thumbnail').not('.code').toggleClass('hidden');
+        $('.works .code').removeClass('hidden');
+      }else{
+        $('.thumbnail .code').not('.nb').removeClass('hidden');
+        $('.thumbnail').not('.code').addClass('hidden');
+      }
+      $(this).toggleClass('activ');
+      $('.categories p').not(this).removeClass('activ');
+      $('.works .code').removeClass('hidden');
+    });
+
+    function resetThumbnailsDisplay() {
+      $('.thumbnail').removeClass('hidden');
+      $('.categories p').removeClass('activ');
+    }
+
+
+    /*GOLD COLOR
+    rgb(198,150,20)
+    rgb(235,209,90)
+    rgb(255,255,255)
+    */
+
+    $('.thumbnail').not('.this').magnificPopup({
+        type: 'ajax',
+        // Delay in milliseconds before popup is removed
+        removalDelay: 500,
+        // Class that is added to popup wrapper and background
+        // make it unique to apply your CSS animations just to this exact popup
+        mainClass: 'mfp-fade',
+        // other options
+        fixedContentPos: true
+    });
+
+});
