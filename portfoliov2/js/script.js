f8da484f7b158ccff2f3f91eae7e79eafdd2ee2e
remydekor <remyj.duval@gmail.com>
2018-06-19 23:07:12 +0200

    started working on new portfolio version

--- portfoliov2/js/script.js
+++ portfoliov2/js/script.js
@@ -1,85 +1,67 @@
 $(document).ready(function() {
 
-      $('#rectListener').on('click', function() {
-          resetThumbnailsDisplay();
-      });
-      $('.this').not('.nb').on('click', function() {
-          resetThumbnailsDisplay();
-      });
+  $('#rectListener').on('click', function() {
+    resetCategorizedDisplay();
+  });
+  $('.this').not('.nb').on('click', function() {
+    resetCategorizedDisplay();
+  });
 
-    $('.categories .illus').on('click', function() {
-      if ($(this).hasClass('activ')) {
-        $('.thumbnail').not('.illus').toggleClass('hidden');
-        $('.works .illus').removeClass('hidden');
+  let categoriesArray = ['.illus', '.design', '.code'];
+
+  let categorizeDisplay = (_class) => {
+    return function(){
+
+      if (!$(this).hasClass('activ')) {  // if this has NOT the activ class
+        $('.thumbnail' + _class).removeClass('hidden'); // div with the .thumbnail AND activated classes
+        $('.thumbnail' + _class).addClass('activ');
+
+        $('.thumbnail').not(_class).not('.nb').addClass('hidden');
+        $('.thumbnail').not(_class).not('.nb').removeClass('activ');
+
+        $(_class + ' .nb').removeClass('hidden');
+        $(_class + ' .nb').addClass('activ');
+        $('.thumbnail').not(_class).children().addClass('hidden');
+        $('.thumbnail').not(_class).children().removeClass('activ');
+
+        console.log('this is '+_class);
+
+        $(this).addClass('activ');
+        $('.categories p').not(this).removeClass('activ');
+
       }else{
-        $('.thumbnail .illus').not('.nb').removeClass('hidden');
-        $('.thumbnail').not('.illus').addClass('hidden');
+        resetCategorizedDisplay();
+
       }
-      $(this).toggleClass('activ');
-      $('.categories p').not(this).removeClass('activ');
-      $('.works .illus').removeClass('hidden');
-    });
+    };
+  };
 
-    $('.categories .photo').on('click', function() {
-      if ($(this).hasClass('activ')) {
-        $('.thumbnail').not('.photo').toggleClass('hidden');
-        $('.works .photo').removeClass('hidden');
-      }else{
-        $('.thumbnail .photo').not('.nb').removeClass('hidden');
-        $('.thumbnail').not('.photo').addClass('hidden');
-      }
-      $(this).toggleClass('activ');
-      $('.categories p').not(this).removeClass('activ');
-      $('.works .photo').removeClass('hidden');
-    });
-
-    $('.categories .design').on('click', function() {
-      if ($(this).hasClass('activ')) {
-        $('.thumbnail').not('.design').toggleClass('hidden');
-        $('.works .design').removeClass('hidden');
-      }else{
-        $('.thumbnail .design').not('.nb').removeClass('hidden');
-        $('.thumbnail').not('.design').addClass('hidden');
-      }
-      $(this).toggleClass('activ');
-      $('.categories p').not(this).removeClass('activ');
-      $('.works .design').removeClass('hidden');
-    });
-
-    $('.categories .code').on('click', function() {
-      if ($(this).hasClass('activ')) {
-        $('.thumbnail').not('.code').toggleClass('hidden');
-        $('.works .code').removeClass('hidden');
-      }else{
-        $('.thumbnail .code').not('.nb').removeClass('hidden');
-        $('.thumbnail').not('.code').addClass('hidden');
-      }
-      $(this).toggleClass('activ');
-      $('.categories p').not(this).removeClass('activ');
-      $('.works .code').removeClass('hidden');
-    });
-
-    function resetThumbnailsDisplay() {
-      $('.thumbnail').removeClass('hidden');
-      $('.categories p').removeClass('activ');
-    }
+  for (let category of categoriesArray) {
+    $('.categories ' + category).click( categorizeDisplay(category) );
+  }
 
 
-    /*GOLD COLOR
-    rgb(198,150,20)
-    rgb(235,209,90)
-    rgb(255,255,255)
-    */
+  function resetCategorizedDisplay() {
+    $('.categories p').removeClass('activ');
+    $('.thumbnail').removeClass('hidden');
+    $('.thumbnail').removeClass('activ');
+  }
 
-    $('.thumbnail').not('.this').magnificPopup({
-        type: 'ajax',
-        // Delay in milliseconds before popup is removed
-        removalDelay: 500,
-        // Class that is added to popup wrapper and background
-        // make it unique to apply your CSS animations just to this exact popup
-        mainClass: 'mfp-fade',
-        // other options
-        fixedContentPos: true
-    });
+  /*GOLD COLOR
+  rgb(120,150,20)
+  rgb(235,210,90)
+  rgb(255,255,255)
+  */
+
+  $('.thumbnail').not('.this').magnificPopup({
+    type: 'ajax',
+    // Delay in milliseconds before popup is removed
+    removalDelay: 500,
+    // Class that is added to popup wrapper and background
+    // make it unique to apply your CSS animations just to this exact popup
+    mainClass: 'mfp-fade',
+    // other options
+    fixedContentPos: true
+  });
 
 });
